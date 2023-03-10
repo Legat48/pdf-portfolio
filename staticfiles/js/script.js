255134e8dcac567ee4fb4cd70b999593f616209d
Legat48 <daletters01@gmail.com>
2023-03-10 20:43:27 +0300

    fix

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -5,19 +5,31 @@ const light_blue = '#0C8DEA';
 const greyTransparent = "#CFD9E67D";
 const text_primary = '#080C42';
 
+
+
 function draw_doughnut(chart_id, valueArr, colorArr) {
     const elem = document.getElementById(chart_id);
     if (!elem) {
         console.error('Graphs with id: ' + chart_id + ' not found!')
         return;
     }
-
+    const dataLabels = {
+        formatter: (e) => e + '%',
+        padding: 15,
+        color: "#080C42",
+        // align: "bottom", // выравнивание
+        anchor: 'end', // выравнивание
+        offset: 115, // смещение
+    }
     return new Chart(document.getElementById(chart_id), {
         type: 'doughnut',
         data: {
             datasets: [{
                 backgroundColor: colorArr,
-                data: valueArr
+                data: valueArr,
+                datalabels: dataLabels,
+                borderColor: 'red',
+                borderWidth: 10
             }]
         },
         options: {
@@ -26,6 +38,9 @@ function draw_doughnut(chart_id, valueArr, colorArr) {
             responsive: true,
             maintainAspectRatio: false,
             rotation: 7
-        }
+        },
+        plugins: [
+            ChartDataLabels,
+        ]
     });
 }
