ca4ef583794fa0ab94d291f5b07820c16133200d
Legat48 <daletters01@gmail.com>
2023-03-14 13:00:49 +0300

    add final

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # pdf-portfolio
+
+Предварительная верстка PDF Portfolio
+
+Плагин для графика - ApexChart
+
+Стили собираются через scc расширением https://marketplace.visualstudio.com/items?itemName=glenn2223.live-sass
+
--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,46 +1,76 @@
-//staticfiles
-const green = '#36C733';
-const blue = '#383E9B';
-const light_blue = '#0C8DEA';
-const greyTransparent = "#CFD9E67D";
-const text_primary = '#080C42';
 
-
-
-function draw_doughnut(chart_id, valueArr, colorArr) {
-    const elem = document.getElementById(chart_id);
-    if (!elem) {
-        console.error('Graphs with id: ' + chart_id + ' not found!')
-        return;
-    }
-    const dataLabels = {
-        formatter: (e) => e + '%',
-        padding: 15,
-        color: "#080C42",
-        // align: "bottom", // выравнивание
-        anchor: 'end', // выравнивание
-        offset: 115, // смещение
-    }
-    return new Chart(document.getElementById(chart_id), {
-        type: 'doughnut',
-        data: {
-            datasets: [{
-                backgroundColor: colorArr,
-                data: valueArr,
-                datalabels: dataLabels,
-                borderColor: 'red',
-                borderWidth: 10
-            }]
-        },
+function polarArea (chartId, valueArr, labelArr, position = 'bottom') {
+  const options = {
+    series: valueArr,
+    labels: labelArr,
+    chart: {
+      type: 'donut',
+    },
+    responsive: [
+      {
         options: {
-            // cutout: '83%',
-            borderWidth: 0,
-            responsive: true,
-            maintainAspectRatio: false,
-            rotation: 7
+          chart: {
+            // width: 200
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      }
+    ]
+  };
+  document.getElementById(chartId)
+  var chart = new ApexCharts(document.getElementById(chartId), options);
+  chart.render();
+}
+
+function polarArea (chartId, valueArr, labelArr, position = 'bottom') {
+  const options = {
+    series: valueArr,
+    labels: labelArr,
+    chart: {
+      type: 'donut',
+    },
+    dataLabels: {
+
+      style: {
+        fontSize: '20px',
+        fontFamily: 'Inter, Arial, sans-serif',
+        fontWeight: 500,
+        colors: ['#1D1D1D'],
+      },
+      dropShadow: {
+        enabled: false,
+      }
+    },
+
+    plotOptions: {
+      pie: {
+        startAngle: 310,
+        endAngle: 670,
+        offsetY: 35,
+        customScale: 0.8,
+        dataLabels: {
+          offset: 74,
+          dropShadow: {
+            enabled: false,
+          }
         },
-        plugins: [
-            ChartDataLabels,
-        ]
-    });
+        donut: {
+          size: '45%',
+        },
+      }
+    },
+    legend: {
+      position: position,
+      horizontalAlign: 'left',
+      fontSize: '12px',
+      fontFamily: 'Inter, Arial, sans-serif',
+      fontWeight: 500,
+    },
+  }
+
+  document.getElementById(chartId)
+  var chart = new ApexCharts(document.getElementById(chartId), options);
+  chart.render();
 }
